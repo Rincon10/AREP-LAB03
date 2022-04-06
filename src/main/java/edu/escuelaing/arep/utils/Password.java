bdd7336843f9d1f27585c3d8468cdc9ba7bc1105
Camilo Rincon <ivan.rincon-s@mail.escuelaing.edu.co>
2022-04-06 00:14:33 -0500

    Password Component

---
+++ src/main/java/edu/escuelaing/arep/utils/Password.java
@@ -0,0 +1,11 @@
+package edu.escuelaing.arep.utils;
+
+/**
+ * @author Iván Camilo Rincón Saavedra
+ * @version 1.0 4/6/2022
+ * @project SecureApp
+ */
+public class Password {
+
+    public static String keyStorePassword = "123456";
+}
